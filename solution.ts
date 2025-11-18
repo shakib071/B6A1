3e8cd22ffc1a87ce7f65db55181504221bfb5bf8
shakib071 <shakibhasan071@gmail.com>
2025-11-18 19:15:08 +0600

    done

--- solution.ts
+++ solution.ts
@@ -166,20 +166,9 @@ const calculateTotalPrice = (products: ProductType[]): number => {
         if(product.discount !== undefined){
             price = price - (price * product.discount) / 100;
         }
-        console.log(price);
+        
         return price;
     }).reduce((sum,val)=> sum+val,0);
 
     return total;
 }
-
-
-const products = [
-  { name: 'Pen', price: 10, quantity: 2 },
-  { name: 'Notebook', price: 25, quantity: 3, discount: 10 },
-  { name: 'Bag', price: 50, quantity: 1, discount: 20 },
-];
-
-console.log(calculateTotalPrice(products));
-
-
